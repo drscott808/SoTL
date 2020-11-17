7aaf6faf10563dc5b1d5830cc70c8705dca388cf
drscott808 <70673795+drscott808@users.noreply.github.com>
2020-11-17 09:18:54 -0500

    correct file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /*** USE THIS FILE TO SET OPTIONS ***/
 
 GexfJS.setParams({
-    graphFile : "testnetwork.gexf",
+    graphFile : "firstvis.gexf",
         /*
             The GEXF file to show ! -- can be overriden by adding
             a hash to the document location, e.g. index.html#celegans.gexf
